7f2f04966671eab21cb5a82630980b4087e727b3
SonglinLyu <lyus@usc.edu>
2023-01-26 20:13:37 -0500

    put box to the center of a cell in grid

--- Assets/Object/Box.cs
+++ Assets/Object/Box.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
+    public Grid floorGrid;
+    Vector3Int cellGridPosition;
+    Vector3 cellCenterLocalPosition;
+    Vector3 cellLocalPosition;
+    Vector3 cellCenterWorldPosition;
+    Vector3 cellWorldPosition;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Box Initiate");
         
+        // GridLayout gridLayout = transform.parent.GetComponent<GridLayout>();
+        // cellGridPosition = floorGrid.LocalToCell(transform.localPosition);
+        // Debug.Log(cellGridPosition);
+        // cellLocalPosition = floorGrid.CellToLocal(cellGridPosition);
+        // cellCenterLocalPosition = floorGrid.GetCellCenterLocal(cellGridPosition);
+        // Debug.Log(cellLocalPosition);
+        // Debug.Log(cellCenterLocalPosition);
+        // Debug.Log(transform.localPosition);
+        // transform.localPosition = cellCenterLocalPosition;
+        // Debug.Log();
+
+        cellGridPosition = floorGrid.WorldToCell(transform.position);
+        Debug.Log(cellGridPosition);
+        cellWorldPosition = floorGrid.CellToLocal(cellGridPosition);
+        cellCenterWorldPosition = floorGrid.GetCellCenterWorld(cellGridPosition);
+        Debug.Log(cellWorldPosition);
+        Debug.Log(cellCenterWorldPosition);
+        Debug.Log(transform.position);
+        transform.position = cellCenterWorldPosition;
     }
 
     // Update is called once per frame
