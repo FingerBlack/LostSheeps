ba7d4b93446401acb64125b9794aa0cafe97bf82
SonglinLyu <lyus@usc.edu>
2023-01-26 23:46:10 -0500

    get collider in c#

--- Assets/Object/Box.cs
+++ Assets/Object/Box.cs
@@ -6,33 +6,24 @@ public class Box : MonoBehaviour
 {
     public Grid floorGrid;
     Vector3Int cellGridPosition;
-    Vector3 cellCenterLocalPosition;
-    Vector3 cellLocalPosition;
     Vector3 cellCenterWorldPosition;
     Vector3 cellWorldPosition;
+    BoxCollider2D collider;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Box Initiate");
-        
-        // GridLayout gridLayout = transform.parent.GetComponent<GridLayout>();
-        // cellGridPosition = floorGrid.LocalToCell(transform.localPosition);
-        // Debug.Log(cellGridPosition);
-        // cellLocalPosition = floorGrid.CellToLocal(cellGridPosition);
-        // cellCenterLocalPosition = floorGrid.GetCellCenterLocal(cellGridPosition);
-        // Debug.Log(cellLocalPosition);
-        // Debug.Log(cellCenterLocalPosition);
-        // Debug.Log(transform.localPosition);
-        // transform.localPosition = cellCenterLocalPosition;
-        // Debug.Log();
+
+        collider = gameObject.GetComponent<BoxCollider2D>();
+        Debug.Log(collider.transform.position);
+        Debug.Log(transform.position);
 
         cellGridPosition = floorGrid.WorldToCell(transform.position);
-        Debug.Log(cellGridPosition);
+        // Debug.Log(cellGridPosition);
         cellWorldPosition = floorGrid.CellToLocal(cellGridPosition);
         cellCenterWorldPosition = floorGrid.GetCellCenterWorld(cellGridPosition);
         Debug.Log(cellWorldPosition);
         Debug.Log(cellCenterWorldPosition);
-        Debug.Log(transform.position);
         transform.position = cellCenterWorldPosition;
     }
 
