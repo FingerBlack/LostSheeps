2a5a7557f7d5b526335e7f6848e364ab6648c043
ccchen <age39303@gmail.com>
2023-01-27 15:47:56 -0800

    week 3 progress

---
+++ Assets/scripts/Plant.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    private Enemy target;
+
+    public GameObject bullet;
+
+    private float shootTimer = 0.0f;
+    public float shootPeriod = 1.0f;
+
+    private Vector3 bulletOffset = new Vector3(0.5f, 1.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target)
+        {
+            // shoot every period of time
+            shootTimer += Time.deltaTime;
+            if(shootTimer > shootPeriod)
+            {
+                shootTimer -= shootPeriod;
+
+                GameObject obj = Instantiate(bullet, transform);
+                obj.transform.position += bulletOffset;
+
+                Debug.Log("Shoot");
+                // bullets should update itself?
+            }
+        }
+        else
+        {
+            // reset timer if there is no enemy
+            shootTimer=0.0f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        // if no target and a new enemy appear inside the range, target on it 
+        if(target==null && collider.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            target = enemy;
+            Debug.Log("target");
+        }
+    }
+}
