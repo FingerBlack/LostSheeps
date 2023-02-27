8db2cd78e5a67a338012e6fe0a33d78ecf6574c5
Joejoequ <455417850@qq.com>
2023-02-26 16:26:12 -0800

    fix

--- Assets/Scripts/Analytics/PushData.cs
+++ Assets/Scripts/Analytics/PushData.cs
@@ -10,12 +10,14 @@ public class PushData
     public string levelTime;
     public string level;
     public string eventTime;
+    public string pushCount;
     public PushData(string userID,string levelTime,string level)
     {
         this.level = level;
         this.levelTime = levelTime;
         this.userID = userID;
         this.eventTime = PlayingStats.printDate(System.DateTime.Now);
+        this.pushCount = "1";
       
     }
 }
