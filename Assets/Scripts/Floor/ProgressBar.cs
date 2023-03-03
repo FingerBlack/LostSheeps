ef6637b3f1b5d94866fdcaa2f9f14fb7d0014f6c
Supkids-C <zchen840@usc.edu>
2023-03-02 21:16:56 -0800

    Modified color and the way progress bar expand

--- Assets/Scripts/Floor/ProgressBar.cs
+++ Assets/Scripts/Floor/ProgressBar.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class ProgressBar : MonoBehaviour
 {
-    public SpriteRenderer progressBar; // reference to the square sprite renderer
-    public CanvasManager canvasManager; // reference to the CanvasManager script
-    public float barHeight = 0f; // the initial width of the progress bar
+    public SpriteRenderer progressBar;      // reference to the square sprite renderer
+    public CanvasManager canvasManager;     // reference to the CanvasManager script
+    public float barHeight = 0f;            
+    public float barWidth = 0f;             // the initial width of the progress bar
     public float captureTime = 3.0f;
-    public float percentage;
-    public float maxhei = 4.4f;
+    private float percentage;
+    public float maxhei;
+    public float maxwidth;
+    Color newColor = new Color(0f, 1f, 0f, 0.5f);
     void Start()
     {
         canvasManager=GameObject.Find("Canvas").GetComponent<CanvasManager>();
         progressBar = GetComponent<SpriteRenderer>();
-         // set the color of the progress bar to green
-        barHeight = 3.5f;
     }
 
     void Update()
@@ -23,13 +24,13 @@ public class ProgressBar : MonoBehaviour
         if(!canvasManager.ifStart){
             return;
         }
-        progressBar.color = Color.green;
+        progressBar.color = newColor;
         percentage = Mathf.Clamp(canvasManager.timeCount ,0 , captureTime) / captureTime; // calculate the progress percentage
         barHeight= maxhei * percentage; // calculate the new width of the progress bar
-        //Debug.Log("this is height right now" + barHeight);
-        //progressBar.size = new Vector2( progressBar.size.x, barHeight; // set the new width of the progress bar
+        barWidth = maxwidth * percentage;
         Vector3 newScale = progressBar.transform.localScale;
         newScale.y = barHeight;
+        newScale.x = barWidth;
         progressBar.transform.localScale = newScale;
     }
 }
